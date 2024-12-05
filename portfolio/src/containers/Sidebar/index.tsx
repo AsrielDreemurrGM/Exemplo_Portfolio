9abcf16402b568267c424b42080e47fe725e0f05
Eduardo Augusto <eduardo.motta.980315@gmail.com>
2024-12-04 23:02:54 -0300

    Estilizado o Sidebar
    - Criado Componente Para o Avatar;
    - Criado Botão Para Trocar Tema da Página;

--- portfolio/src/containers/Sidebar/index.tsx
+++ portfolio/src/containers/Sidebar/index.tsx
@@ -1,9 +1,22 @@
+import Avatar from '../../components/Avatar'
+import Paragraph from '../../components/Paragraph'
 import Titulo from '../../components/Title'
+
+import { Description, ThemeButton, SidebarContainer } from './styles'
 
 const Sidebar = () => (
   <aside>
-    <img src="https://github.com/AsrielDreemurrGM.png" alt="" />
-    <Titulo fontSize={20}>Eduardo Augusto</Titulo>
+    <SidebarContainer>
+      <Avatar />
+      <Titulo fontSize={20}>Eduardo Augusto</Titulo>
+      <Paragraph type="secondary" fontSize={16}>
+        AsrielDreemurrGM
+      </Paragraph>
+      <Description type="primary" fontSize={12}>
+        Dev Full Stack Java
+      </Description>
+      <ThemeButton>Trocar Tema</ThemeButton>
+    </SidebarContainer>
   </aside>
 )
 
