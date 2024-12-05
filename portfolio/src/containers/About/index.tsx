505541e6aeaded37047ba96cbdb9d7426dc99a22
Eduardo Augusto <eduardo.motta.980315@gmail.com>
2024-12-04 23:52:12 -0300

    Atualizado Informações

--- portfolio/src/containers/About/index.tsx
+++ portfolio/src/containers/About/index.tsx
@@ -6,16 +6,18 @@ const Sobre = () => (
   <section>
     <Title fontSize={16}>Sobre Mim:</Title>
     <Paragraph type="primary">
-      Sou desenvolvedor web com experiência em HTML5, CSS, Bootstrap, GULP,
-      SASS, LESS e GRUNT, além de estar aprimorando minhas habilidades em
-      JavaScript e frameworks modernos como React. Meu objetivo é trabalhar
-      remotamente a partir do Brasil. Atualmente, estou criando projetos que
-      demonstram minhas competências e acredito que o LinkedIn será uma
-      ferramenta essencial para conquistar meu primeiro emprego na área de
-      tecnologia. Sou proativo e focado em me desenvolver continuamente, com um
-      forte interesse em ferramentas modernas e boas práticas de
-      desenvolvimento. Estou sempre aberto a novos desafios e motivado a
-      colaborar em equipes que valorizam a criatividade e a eficiência.
+      Sou Desenvolvedor Web com experiência em HTML, CSS, JavaScript, Bootstrap,
+      GULP, SASS, SCSS, LESS, GRUNT, VueJS, React com JS e TypeScript e Create
+      React App além de estar aprimorando minhas habilidades em JavaScript e
+      frameworks modernos como React. Meu objetivo é finalizar o Curso de
+      Desenvolvedor Java Full Stack e trabalhar remotamente do Brasil.
+      Atualmente, estou criando projetos que demonstram minhas competências e
+      acredito que o LinkedIn será uma ferramenta essencial para conquistar meu
+      primeiro emprego na área de tecnologia. Sou proativo e focado em me
+      desenvolver continuamente, com um forte interesse em ferramentas modernas
+      e boas práticas de desenvolvimento. Estou sempre aberto a novos desafios e
+      motivado a colaborar em equipes que valorizam a criatividade e a
+      eficiência.
     </Paragraph>
     <GithubSection>
       <img src="https://github-readme-stats.vercel.app/api?username=AsrielDreemurrGM&show_icons=true&theme=dracula&include_all_commits=true&count_private=true" />
